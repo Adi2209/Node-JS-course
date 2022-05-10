10aee04e55009c347ffc437cac07b8f75a8f58a7
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2022-05-11 01:31:19 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-const http = require('http');
+const path = require('path');
 
-const routes = require('./routes')
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressHbs=require('express-handlebars')
 
-const server = http.createServer(routes);
+const app = express();
+//use allows us to add a middleware function ;
 
 
-server.listen(3000);//node js will keep listening for incoming events
+app.engine('hbs',expressHbs({layoutsDir:'views/layouts/',defaultLayout:'main-layout',extname:'hbs'}));
+app.set('view engine','hbs');//telling express that we have to compile dynamic template(html) with pug engine 
+app.set('views','views');//telling express to where to find the templates
+
+const adminData=require('./routes/admin')
+const shopRoutes=require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/admin',adminData.routes);
+
+app.use(shopRoutes);
+
+app.use((req,res,next)=>{
+    res.status(404).render('404',{pageTitle:'Page Not Found'});
+
+})
+
+app.listen(3000);//node js will keep listening for incoming events
+
